e7b0ce7d99f7ef690e7a7c17a5e23743ccedaa36
shailesh-deore <shaileshdeore557@gmail.com>
2026-06-10 16:28:36 +0530

    Add Basics11

---
+++ src/module01_basics/Basics11.java
@@ -0,0 +1,32 @@
+package module01_basics;
+
+public class Basics11 {
+    static void main(String[] args) {
+        // division deep dive
+        // if you get both num int then answer or quotient also in integer
+        //like
+        int num = 10;
+        int num2 = 3;
+        int i = num / num2;
+        System.out.println(i);
+        // int me hi Aayega 3
+
+       // but here if you get double then variable b promoted in double automatically
+        double a = 10 ;
+        int b = 3;
+        double v = a / b;
+        System.out.println(v);
+       // output : 3.3333333333333335
+      // similarly
+        float n1 = 10.0f;
+        int n2 = 3;
+        float v1 = n1 / n2;
+        System.out.println(v1);
+        // widening conversion
+
+
+
+
+
+    }
+}
