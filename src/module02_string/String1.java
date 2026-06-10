546df2f78744e8a269faa9bf28b84e15e4279b75
shailesh-deore <shaileshdeore557@gmail.com>
2026-06-10 11:25:23 +0530

    Add String1

---
+++ src/module02_string/String1.java
@@ -0,0 +1,16 @@
+package module02_string;
+
+public class String1 {
+    static void main(String[] args) {
+
+        // STRING DATA TYPE: THE BASICS
+
+        // String is a Non-Primitive Data Type. It is a built-in class in Java.
+        // A String is a sequence of characters enclosed in double quotes ("").
+        String name = "Shailesh";
+        System.out.println(name);
+
+
+
+    }
+}
