46c2ff1e8c77f5a355cab3874727357201346e2d
shailesh-deore <shaileshdeore557@gmail.com>
2026-06-15 21:17:11 +0530

    Add Student.java String basics

---
+++ src/module02_string/Student.java
@@ -0,0 +1,13 @@
+package module02_string;
+
+public class Student {
+
+      String name;
+      String address;
+      int standard;
+      int roleNumber;
+
+      // i create this is blueprint
+
+
+}
