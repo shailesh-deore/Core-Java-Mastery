3decb1c25f7e6234014c142aa002af2c8306630b
shailesh-deore <shaileshdeore557@gmail.com>
2026-06-21 16:17:01 +0530

    Add String basics java file

---
+++ src/module02_string/String14.java
@@ -0,0 +1,12 @@
+package module02_string;
+
+public class String14 {
+    static void main(String[] args) {
+        // Rule: String.valueOf() converts a primitive int into a real String object.
+        int a = 10;
+        String s = String.valueOf(a);
+        System.out.println(s); // Output: "10"
+
+        // now 10 is a string we can use all string method with s.
+    }
+}
