0adccef8a717e0fac8780f4ca5f7087d5d93d7b9
shailesh-deore <shaileshdeore557@gmail.com>
2026-06-26 09:29:19 +0530

    Add a basic file of loops

---
+++ src/module04_loops/Loops05.java
@@ -0,0 +1,13 @@
+package module04_loops;
+
+public class Loops05 {
+    static void main(String[] args) {
+        // for loop example for good understanding !
+        // table of 7
+
+        for(int i = 1 ; i <= 10 ; i++){
+            System.out.println(i + " X 7 = " + i * 7);
+        }
+
+    }
+}
