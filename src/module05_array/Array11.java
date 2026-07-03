fda628fd3fa19fcc535bbe07c7988c12c8b6ebfd
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-03 10:27:48 +0530

    Add a basic Array file of java programming

---
+++ src/module05_array/Array11.java
@@ -0,0 +1,27 @@
+package module05_array;
+
+public class Array11 {
+    static void main(String[] args) {
+        // 2D Array (Two Dimensional Array)
+        // define :
+
+        // syntax
+        int[][] arr = new int [3][3]; // two index
+
+        /*
+        arr:
+        +---+---+---+
+        | 0 | 0 | 0 |    arr[0]
+        +---+---+---+
+        | 0 | 0 | 0 |    arr[1]
+        +---+---+---+
+        | 0 | 0 | 0 |    arr[2]
+        +---+---+---+
+
+         */
+        // 3 by 3 matrix ( 3 rows & 3 column )
+
+
+
+    }
+}
