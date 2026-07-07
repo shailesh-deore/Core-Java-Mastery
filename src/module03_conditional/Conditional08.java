1ac46abdd322c4c6d17d4b5bc146dd5bf1420020
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-07 17:43:04 +0530

    updated file with code

--- src/module03_conditional/Conditional08.java
+++ src/module03_conditional/Conditional08.java
@@ -2,6 +2,23 @@ package module03_conditional;
 
 public class Conditional08 {
     static void main(String[] args) {
-        //
+        //Problem Statement:
+        // Write a program that simulates a traffic light and tells the driver what to do.
+
+        String lightColor = "yellow";
+
+        if(lightColor.equals("Red")){
+            System.out.println("Stop!");
+
+        } else if(lightColor.equals("yellow")) {
+            System.out.println("Slow down!");
+
+        } else if(lightColor.equals("Green")) {
+            System.out.println("Go!");
+        }else{
+            System.out.println("Invalid traffic light color.");
+        }
+
+        }
     }
-}
+
