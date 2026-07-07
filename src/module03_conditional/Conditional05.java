a911a12aa280de70525ba05f8617a981c35d7385
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-07 17:22:17 +0530

    Add a basic problem solving file of java

---
+++ src/module03_conditional/Conditional05.java
@@ -0,0 +1,17 @@
+package module03_conditional;
+
+public class Conditional05 {
+    static  void  main(String[]args){
+        // pass fail simple code with conditional ok
+
+        int myScore = 75;
+
+        if(myScore >= 50){
+            System.out.println("you are pass !");
+        }else{
+            System.out.println("sorry you are failed !");
+        }
+
+
+    }
+}
