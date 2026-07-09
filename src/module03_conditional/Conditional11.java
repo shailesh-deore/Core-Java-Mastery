089fbdbf9ab7d7c38bd6ee3c44b2f24b7dd77026
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-09 22:49:03 +0530

    Add a basic conditional java file

---
+++ src/module03_conditional/Conditional11.java
@@ -0,0 +1,17 @@
+package module03_conditional;
+
+public class Conditional11 {
+    static void main(String[] args) {
+
+                int num1 = 45;
+                int num2 = 82;
+
+                if (num1 > num2) {
+                    System.out.println(num1 + " is greater than " + num2);
+                } else {
+                    System.out.println(num2 + " is greater than " + num1);
+                }
+            }
+        }
+
+
