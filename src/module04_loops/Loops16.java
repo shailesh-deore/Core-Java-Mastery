e404a389865e1e38de6c95ad2d98383853b073d9
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-09 22:49:48 +0530

    Add a Loops16 file

---
+++ src/module04_loops/Loops16.java
@@ -0,0 +1,20 @@
+package module04_loops;
+
+public class Loops16 {
+    static void main(String[] args) {
+
+        System.out.println("Rocket preparing for takeoff...");
+
+        // This loop starts at 5 and counts down to 1
+        for (int i = 5; i >= 1; i--) {
+            System.out.println("T-minus " + i + " seconds...");
+        }
+
+        // This runs after the loop finishes
+        System.out.println("🚀 BLAST OFF! The rocket has launched!");
+    }
+}
+
+
+
+
