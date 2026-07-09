c484891303572933b8380165d57892f6651fc411
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-09 22:48:27 +0530

    Add a basic conditional java file

---
+++ src/module03_conditional/Conditional10.java
@@ -0,0 +1,14 @@
+package module03_conditional;
+
+public class Conditional10 {
+    static void main(String[] args) {
+        int number = -5; // Change this to test other numbers
+
+        if (number >= 0) {
+            System.out.println("The number is POSITIVE.");
+        } else {
+            System.out.println("The number is NEGATIVE.");
+        }
+    }
+
+}
