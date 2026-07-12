fa4ced6a57cf064026d3d6e882d7138798941b80
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-12 18:54:11 +0530

    Add a basic opr file

---
+++ src/module01_basics/Basics29.java
@@ -0,0 +1,58 @@
+package module01_basics;
+
+public class Basics29 {
+    static void main(String[] args) {
+
+   /*
+
+   RELATIONAL OPERATORS (Java)
+   //Documentation
+
+
+   Definition:
+   Relational operators compare two values or expressions.
+   The result is always a boolean value (true or false).
+
+   Operators:
+   ==   Equal to
+   !=   Not equal to
+   >    Greater than
+   <    Less than
+   >=   Greater than or equal to
+   <=   Less than or equal to
+
+   Used With:
+   1. Primitive data types
+   -> Compares actual values.
+
+   Example:
+   int a = 10;
+   int b = 20;
+   System.out.println(a < b);   // true
+
+   2. Objects (Reference Types)
+   -> '==' compares object references (memory addresses),
+      NOT object contents.
+
+   Example:
+   String s1 = "Java";
+   String s2 = new String("Java");
+
+   System.out.println(s1 == s2);      // false
+   System.out.println(s1.equals(s2)); // true
+
+   Important:
+   ==       -> Compares values (primitive) OR references (objects).
+   .equals() -> Compares object contents (mainly String and other objects).
+
+   Shortcut:
+   Primitive + ==  -> Value Compare
+   Object + ==     -> Reference Compare
+   Object + equals() -> Content Compare
+
+
+   */
+
+
+    }
+}
