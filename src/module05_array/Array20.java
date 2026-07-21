e7dbe82ec57176b59f01ee31a95d50e087537c94
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-21 13:08:29 +0530

    Add a 2d array example file

---
+++ src/module05_array/Array20.java
@@ -0,0 +1,22 @@
+package module05_array;
+
+public class Array20 {
+    static void main(String[] args) {
+
+        int[][]arr = new int [2][3];
+        // fill elements through index
+        arr[0][0]=10;
+        arr[0][1]=20;
+        arr[0][2]=30;
+        arr[1][0]=40;
+        arr[1][1]=50;
+        arr[1][2]=60;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
