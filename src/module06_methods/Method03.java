6a0b75b1fb72993403dbc07ad4ed484ca9e40873
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-23 18:18:19 +0530

    Add a basic file of method of java

---
+++ src/module06_methods/Method03.java
@@ -0,0 +1,33 @@
+package module06_methods;
+
+public class Method03 {
+    static void main(String[] args) {
+
+        /*
+------------------------------------------------------------
+Method Naming Convention
+------------------------------------------------------------
+
+1. Method name should start with a lowercase letter.
+2. Use camelCase naming.
+3. Name should describe the work of the method.
+4. Always use meaningful names.
+
+Good Examples
+
+calculateSum()
+findMaximum()
+printTable()
+checkPrime()
+getStudentName()
+
+Bad Examples
+
+abc()
+xyz()
+m1()
+test()
+
+*/
+    }
+}
