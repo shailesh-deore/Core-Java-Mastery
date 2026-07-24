f4bceff61029cd7b5c746ea5ec5e3086720cf52d
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-24 11:10:31 +0530

    Add a basic of method

---
+++ src/module06_methods/Method05.java
@@ -0,0 +1,34 @@
+package module06_methods;
+
+public class Method05 {
+    static void main(String[] args){
+        /*
+============================================================
+HOW TO CHOOSE METHOD TYPE
+============================================================
+
+Question 1
+
+Does the method need input?
+
+YES -> Parameter
+
+NO -> No Parameter
+
+--------------------------------------------
+
+Question 2
+
+Does the caller need a result?
+
+YES -> Return
+
+NO -> void
+
+After answering these two questions,
+the method type is automatically decided.
+
+============================================================
+*/
+  }
+}
