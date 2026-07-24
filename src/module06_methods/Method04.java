a01df5ae4f04cd26f7046272cba28db467b50ed8
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-24 11:08:57 +0530

    Add basic of method

---
+++ src/module06_methods/Method04.java
@@ -0,0 +1,32 @@
+package module06_methods;
+
+public class Method04 {
+    static void main(String[] args) {
+        /*
+------------------------------------------------------------
+void vs return
+------------------------------------------------------------
+
+void
+----
+- Performs a task.
+- Does not return any value.
+
+Example:
+printTable();
+
+--------------------------------------------
+
+return
+------
+- Sends a value back to the caller.
+- Caller can store or print the returned value.
+
+Example:
+
+int result = square(5);
+
+*/
+
+    }
+}
