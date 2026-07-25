5c300a2e3380f6be7ddb753d5ef89e6ecd876531
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-25 19:31:28 +0530

    Add a basic of method

---
+++ src/module06_methods/Method10.java
@@ -0,0 +1,15 @@
+package module06_methods;
+// Percentage = (obtainedMarks × 100) / totalMarks
+public class Method10 {
+
+    // calculate student percentage with using standard percentage formula
+    static  float calculatePercentage(int obtMarks, int totalMarks ){
+
+        return  ((float)obtMarks *100)/totalMarks;
+    }
+    static void main(String[] args) {
+
+        float res = calculatePercentage(405,500);
+        System.out.println(res);
+    }
+}
