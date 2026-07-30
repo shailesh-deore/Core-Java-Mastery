abff4ebcc35873fcc71e33f55837b514b9fea0ca
shailesh-deore <shaileshdeore557@gmail.com>
2026-07-30 09:23:57 +0530

    Add a methods

---
+++ src/module06_methods/Method17.java
@@ -0,0 +1,29 @@
+package module06_methods;
+
+public class Method17 {
+
+    static int sumOfArray(int[] arr){
+        int res = 0 ;
+        for(int x : arr){
+            res += x;
+        }
+        return res;
+    }
+    private static String ToUpper(String str){
+        return str.trim().toUpperCase();
+    }
+    public static void main(String[] args) {
+
+        int[] a = {10,20,30};
+        int [] b = {10,20,30,40,50};
+
+
+        int res = sumOfArray(a);
+        int res1 = sumOfArray(b);
+
+        System.out.println(res);
+        System.out.println(res1);
+
+        System.out.println(ToUpper("  vipul   "));
+    }
+}
