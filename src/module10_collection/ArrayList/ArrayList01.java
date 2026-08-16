8b562279a78a85951697efa99678f4446d9f4243
shailesh-deore <shaileshdeore557@gmail.com>
2026-08-16 23:02:56 +0530

    Add a collections basics

---
+++ src/module10_collection/ArrayList/ArrayList01.java
@@ -0,0 +1,16 @@
+package module10_collection.ArrayList;
+import java.util.ArrayList;
+
+public class ArrayList01 {
+    public static void main(String[] args) {
+
+        // ArrayList : Class used to store multiple Integer objects dynamically.
+        // <Integer> : Generic that tells Java to store Integer objects only.
+        // marks : Reference variable that refers to the ArrayList object.
+        // new : Creates a new object.
+        // ArrayList<>() : Constructor that creates an ArrayList object in heap memory.
+
+        ArrayList<Integer> marks = new ArrayList<>();
+
+    }
+}
