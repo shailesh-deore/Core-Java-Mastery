619603a93275b3348f009eefcb1c75fd5e66b2a6
shailesh-deore <shaileshdeore557@gmail.com>
2026-08-21 21:35:19 +0530

    Add a basics of java

---
+++ src/module10_collection/CollectionFramework.java
@@ -0,0 +1,148 @@
+package module10_collection;
+
+/*
+==============================================================
+                COLLECTION FRAMEWORK - BASIC NOTES
+==============================================================
+
+1) What is Collection Framework?
+--------------------------------
+Collection Framework is a group of ready-made Classes and
+Interfaces provided by Java to store and manage multiple
+objects efficiently.
+
+In simple words:
+It helps us store, search, insert, update and delete data
+easily using built-in methods.
+
+==============================================================
+
+2) Why Collection Framework?
+----------------------------
+Arrays have some limitations:
+
+✔ Fixed Size
+✔ Cannot grow automatically
+✔ More manual coding
+✔ Insertion and deletion are difficult
+✔ No ready-made methods
+
+To solve these problems, Java introduced the Collection Framework.
+
+==============================================================
+
+3) Advantages of Collection Framework
+-------------------------------------
+
+✔ Dynamic Size
+✔ Less Code
+✔ Ready-made Methods
+✔ Better Code Reusability
+✔ Easy Data Management
+✔ Improves Productivity
+
+==============================================================
+
+4) Collection Framework Contains
+--------------------------------
+
+1. Interfaces
+2. Classes
+
+Interfaces define the rules.
+Classes provide the implementation.
+
+Example:
+
+Interface
+---------
+List
+Set
+Map
+
+Classes
+-------
+ArrayList
+LinkedList
+HashSet
+TreeSet
+HashMap
+
+==============================================================
+
+5) Our Learning Roadmap
+-----------------------
+
+Collection Framework
+
+↓
+
+ArrayList
+(HashSet)
+(HashMap)
+
+These three classes are enough to build a strong foundation.
+
+==============================================================
+
+6) Difference Between Array and ArrayList
+-----------------------------------------
+
+Array
+-----
+✔ Fixed Size
+✔ Stores primitive and object data
+✔ No built-in methods
+
+ArrayList
+---------
+✔ Dynamic Size
+✔ Stores Objects
+✔ Many built-in methods
+
+==============================================================
+
+7) Important Keywords
+---------------------
+
+class
+interface
+implements
+extends
+new
+
+==============================================================
+
+8) Important Packages
+---------------------
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+
+==============================================================
+
+9) Remember
+-----------
+
+Collection Framework
+
+↓
+
+Ready-made Classes + Interfaces
+
+↓
+
+Used to store and manage multiple objects efficiently.
+
+==============================================================
+*/
+
+public class CollectionFramework {
+
+    public static void main(String[] args) {
+
+        System.out.println("Collection Framework Notes");
+
+    }
+}
