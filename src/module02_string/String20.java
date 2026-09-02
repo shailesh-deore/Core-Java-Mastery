02fbc18d8f74739e3b05da6d038c9618948ddcfb
shailesh-deore <shaileshdeore557@gmail.com>
2026-09-02 22:40:57 +0530

    Add a String file in java

---
+++ src/module02_string/String20.java
@@ -0,0 +1,17 @@
+package module02_string;
+
+public class String20 {
+    public static void main(String[] args) {
+        String word = "radar";
+
+        // Reverse the word using StringBuilder
+        String reversedWord = new StringBuilder(word).reverse().toString();
+
+        // Simple if-else check
+        if (word.equals(reversedWord)) {
+            System.out.println("Unique! This word is a palindrome.");
+        } else {
+            System.out.println("Regular word. It is not a palindrome.");
+        }
+    }
+}
